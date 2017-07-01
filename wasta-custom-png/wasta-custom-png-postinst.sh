8bdf4d73372713ff86b593b27230922e35e6ffd8
Kim Blewett <kim_blewett@sil.org>
2017-07-01 14:34:34 -0500

    initial xenial support

--- wasta-custom-png/wasta-custom-png-postinst.sh
+++ wasta-custom-png/wasta-custom-png-postinst.sh
@@ -34,15 +34,55 @@ echo
 echo "*** Beginning wasta-custom-png-postinst.sh"
 echo
 
+# setup directory for reference later
+DIR=/usr/share/wasta-custom-png/resources
+
 # ------------------------------------------------------------------------------
-# LibreOffice Preferences Extension Install (for all users)
+# LibreOffice Preferences Extension install (for all users)
 # ------------------------------------------------------------------------------
+
+# REMOVE "Wasta-English-Intl-Defaults" extension: remove / reinstall is only
+#   way to update
+EXT_FOUND=$(ls /var/spool/libreoffice/uno_packages/cache/uno_packages/*/wasta-english-intl-defaults.oxt* 2> /dev/null)
+
+if [ "$EXT_FOUND" ];
+then
+    unopkg remove --shared wasta-english-intl-defaults.oxt
+fi
 
 # Install wasta-english-intl-defaults.oxt (Default LibreOffice Preferences)
 echo
-echo "*** Installing LibreOffice Default Setttings Extension (for all users)"
+echo "*** Installing/Updating Wasta English Intl Default LO Extension"
 echo
-unopkg add --shared /usr/share/wasta-custom-png/resources/wasta-english-intl-defaults.oxt
+unopkg add --shared $DIR/wasta-english-intl-defaults.oxt
+
+# IF user has not initialized LibreOffice, then when adding the above shared
+#   extension, the user's LO settings are created, but owned by root so
+#   they can't change them: solution is to just remove them (will get recreated
+#   when user starts LO the first time).
+
+
+for LO_FOLDER in /home/*/.config/libreoffice;
+do
+    LO_OWNER=$(stat -c '%U' $LO_FOLDER)
+
+    if [ "$LO_OWNER" == "root" ];
+    then
+        echo
+        echo "*** LibreOffice settings owned by root: resetting"
+        echo "*** Folder: $LO_FOLDER"
+        echo
+    
+        rm -rf $LO_FOLDER
+    fi
+done
+
+# ------------------------------------------------------------------------------
+# Set system-wide Paper Size
+# ------------------------------------------------------------------------------
+# Note: This sets /etc/papersize.  However, many apps do not look at this
+#   location, but instead maintain their own settings for paper size :-(
+paperconfig -p a4
 
 # ------------------------------------------------------------------------------
 # Finished
